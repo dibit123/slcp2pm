f7521cfb66fcbe2e3dbad76cb13641a56c00f944
shitong01 <stchin@ntu.edu.sg>
2019-06-08 00:08:22 +0800

    EOS fix LAR looks metadata (#1)
    * make LAR reflect the correct looks used to process data in metadata
    
    * fix int to str issue

--- script/slcp2lar_S1.sh
+++ script/slcp2lar_S1.sh
@@ -5,6 +5,9 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 # inputs
 slcp_dir=$(readlink -f $1)
 swath=$2
+rlks=$3
+alks=$4
+
 dirm=${slcp_dir}/master/IW${swath}
 dirf=${slcp_dir}/fine_coreg/IW${swath}
 dirg=${slcp_dir}/geom_master/IW${swath}
@@ -14,5 +17,5 @@ work_dir=$(pwd)
 mkdir s${swath}
 cd s${swath}
 
-$BASE_PATH/log_ratio.py -mdir ${dirm} -sdir ${dirf} -gdir ${dirg} -rlks 7 -alks 2
+$BASE_PATH/log_ratio.py -mdir ${dirm} -sdir ${dirf} -gdir ${dirg} -rlks ${rlks} -alks ${alks}
 cd $work_dir

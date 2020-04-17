e1fdc78bafe3cdc80fd5bd784d1baf757251dfa3
shitong01 <stchin@ntu.edu.sg>
2020-04-17 19:09:12 +0800

    looks like it only works this way

--- script/create_lar.sh
+++ script/create_lar.sh
@@ -7,7 +7,7 @@ SWATH=$2
 echo "##########################################" 1>&2
 echo -n "Running S1 log amp ratio generation: " 1>&2
 date 1>&2
-python3 $BASE_PATH/create_lar.py $SLCP_PROD > create_lar.log 2>&1
+source /opt/isce2/isce_env.sh && python3 $BASE_PATH/create_lar.py $SLCP_PROD > create_lar.log 2>&1
 STATUS=$?
 echo -n "Finished running S1 log amp ratio generation: " 1>&2
 date 1>&2
